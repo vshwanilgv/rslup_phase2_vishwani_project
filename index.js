7c5d7876b7af687d87a55d83cfac48c14d7881ec
vshwanilgv <VishwaniLGV@gmail.com>
2023-10-12 13:45:28 +0530

    created index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.get('/flights', (req, res) => {
+ 
+});
+
+app.post('/flights', (req, res) => {
+  
+});
+
+
+
+app.listen(3000, () => {
+  console.log('Flight management app is running on port 3000');
+});
