99963530b6380dad1499dcb884617160bc93f082
vshwanilgv <vishwanibhagya2002@gmail.com>
2024-01-02 23:40:10 +0530

    added authentication

--- index.js
+++ index.js
@@ -1,5 +1,81 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+const users = [
+  { id: 1, username: 'user1', password: 'password1' },
+  { id: 2, username: 'user2', password: 'password2' },
+];
+
+
+const secretKey = 'yourSecretKey';
+
+const authenticate = (req, res, next) => {
+  const token = req.header('Authorization');
+  if (!token) return res.status(401).json({ message: 'Unauthorized' });
+
+  jwt.verify(token, secretKey, (err, user) => {
+    if (err) return res.status(403).json({ message: 'Forbidden' });
+    req.user = user;
+    next();
+  });
+};
+
+
+// Create a new user
+app.post('/users', authenticate, (req, res) => {
+  const { username, password } = req.body;
+  const newUser = { id: users.length + 1, username, password };
+  users.push(newUser);
+  res.status(201).json(newUser);
+});
+
+// Update a user by ID
+app.put('/users/:id', authenticate, (req, res) => {
+  const userId = parseInt(req.params.id);
+  const userIndex = users.findIndex((u) => u.id === userId);
+
+  if (userIndex === -1) {
+    return res.status(404).json({ message: 'User not found' });
+  }
+
+  const updatedUser = { ...users[userIndex], ...req.body };
+  users[userIndex] = updatedUser;
+
+  res.json(updatedUser);
+});
+
+
+// Delete a user by ID
+app.delete('/users/:id', authenticate, (req, res) => {
+  const userId = parseInt(req.params.id);
+  const userIndex = users.findIndex((u) => u.id === userId);
+
+  if (userIndex === -1) {
+    return res.status(404).json({ message: 'User not found' });
+  }
+
+  const deletedUser = users.splice(userIndex, 1)[0];
+  res.json(deletedUser);
+});
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  const user = users.find((u) => u.username === username && u.password === password);
+
+  if (!user) {
+    return res.status(401).json({ message: 'Invalid credentials' });
+  }
+
+  const token = jwt.sign({ id: user.id, username: user.username }, secretKey);
+  res.json({ user: { id: user.id, username: user.username }, token });
+});
+
+
 
 app.get('/flights', (req, res) => {
  
@@ -11,6 +87,6 @@ app.post('/flights', (req, res) => {
 
 
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('Flight management app is running on port 3000');
 });
