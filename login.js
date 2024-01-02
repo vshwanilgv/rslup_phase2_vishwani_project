a228fb30887211f087e3c7749cf006fec846634b
vshwanilgv <vishwanibhagya2002@gmail.com>
2024-01-02 23:28:22 +0530

    added CRUD points

---
+++ login.js
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <title>Login page</title>
+        <style>
+             
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <label for="uname"><b>Username</b></label>
+            <input type="text" placeholder="Enter Username" name="uname" required>
+
+            </input>
+            <label for="psw"><b>Password</b></label>
+            <input type="password" placeholder="Enter Password" name="psw" required></input>
+            <button type="submit">Login</button>
+            
+        </div>
+    </body>
+</html>
