ca82c28180b5a3d443f608ac6a1f23fe52447171
vshwanilgv <vishwanibhagya2002@gmail.com>
2024-01-04 13:06:33 +0530

    Added forgot password page

---
+++ forgotPassword.js
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <title>Forgot Password</title>
+        <style>
+             
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <label for="uname"><b>email</b></label>
+            <input type="text" placeholder="Enter email" name="uname" required></input>
+
+           
+            <button type="submit">Reset Password</button>
+            
+        </div>
+    </body>
+</html>
